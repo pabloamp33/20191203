18d6acea860944f14b5781faf9e191748dba378d
alumno <alumno@A209P26.informatica.cifpaviles.pa>
2019-12-03 17:55:14 +0100

    Añadido ejercicio 1

--- src/fp/daw/examen/Ejercicio1.java
+++ src/fp/daw/examen/Ejercicio1.java
@@ -34,18 +34,139 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
-		int numero=0;
+		int numero=0,millar=0,centena=0,decena=0,unidad=0,digito=0,cociente=0; 
+		String romano="";
 		do {
 			System.out.println("Introduzca un número menor o igual a 3000");
 			numero=teclado.nextInt();
 		} while (numero<0 || numero>3000);
 		
-
-
-	    
-	        System.out.println("Error");
-
-	    
+		teclado.close();
+		cociente=numero;
+		while (cociente > 0 )
+		{
+			digito= cociente%10;
+			cociente= cociente/10;
+			if (cociente>99) {
+				unidad=digito;
+			} else {
+				if (cociente>9) {
+					decena=digito;
+				} else {
+					if (cociente>0) {
+						centena=digito;
+					} else {
+						millar=digito;
+					}
+				}
+			}
+			
+		}
+		switch(millar) {
+		  case 1:
+			  romano=romano+"M";
+			  break;
+		  case 2:
+			  romano=romano+"MM";
+			  break;
+		  case 3:
+			  romano=romano+"MMM";
+			  break;
+		  default:
+			  romano=romano+"";
+			  break;
+		 }
+		
+		switch(centena) {
+		  case 1:
+			  romano=romano+"C";
+			  break;
+		  case 2:
+			  romano=romano+"CC";
+			  break;
+		  case 3:
+			  romano=romano+"CCC";
+			  break;
+		  case 4:
+			  romano=romano+"CD";
+			  break;
+		  case 5:
+			  romano=romano+"D";
+			  break;
+		  case 6:
+			  romano=romano+"DC";
+			  break;
+		  case 7:
+			  romano=romano+"DCC";
+			  break;
+		  case 8:
+			  romano=romano+"DCCC";
+			  break;
+		  case 9:	
+			  romano=romano+"CM";
+		      break;
+		}
+		
+		switch(decena) {
+		  case 1:
+			  romano=romano+"X";
+			  break;
+		  case 2:
+			  romano=romano+"XX";
+			  break;
+		  case 3:
+			  romano=romano+"XXX";
+			  break;
+		  case 4:
+			  romano=romano+"XL";
+			  break;
+		  case 5:
+			  romano=romano+"L";
+			  break;
+		  case 6:
+			  romano=romano+"LX";
+			  break;
+		  case 7:
+			  romano=romano+"LXX";
+			  break;
+		  case 8:
+			  romano=romano+"LXXX";
+			  break;
+		  case 9:	
+			  romano=romano+"XC";
+		      break;
+		}
+		
+		switch(unidad) {
+		  case 1:
+			  romano=romano+"I";
+			  break;
+		  case 2:
+			  romano=romano+"II";
+			  break;
+		  case 3:
+			  romano=romano+"III";
+			  break;
+		  case 4:
+			  romano=romano+"IV";
+			  break;
+		  case 5:
+			  romano=romano+"V";
+			  break;
+		  case 6:
+			  romano=romano+"VI";
+			  break;
+		  case 7:
+			  romano=romano+"VII";
+			  break;
+		  case 8:
+			  romano=romano+"VIII";
+			  break;
+		  case 9:	
+			  romano=romano+"IX";
+		      break;
+		}
+		  System.out.println("El número "+numero+" pasado a romanos es:"+romano );    
 		
 		
 	}
